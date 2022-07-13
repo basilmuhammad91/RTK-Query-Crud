f92235e8bac2abfb5163bd4395e22e7ebffe9859
basilmuhammad91 <basilmuhammad91@gmail.com>
2022-07-13 22:58:01 +0500

    Initial Commit

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const PostsList = () => {
+  return (
+    <View>
+      PostsList
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
